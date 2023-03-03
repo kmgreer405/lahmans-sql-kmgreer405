4abbdb00dc44519ae87da221ca23ab07ae7b1b4d
Kevin Greer <kmgreer405@hotmail.com>
2023-03-02 22:16:53 -0500

    Through question 6

--- scripts/lahmans_baseball.sql
+++ scripts/lahmans_baseball.sql
@@ -74,7 +74,19 @@ ORDER BY decade
 --The strikeouts have gone up since the 1920's peaking around 16.2 in the 1960's a game. homeruns have always been below two decimal places per game.
 
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
-	
+WITH my_cte AS (
+SELECT DISTINCT playerid AS player,
+sb + cs AS sb_attempts,
+sb
+FROM batting
+WHERE yearid = 2016
+AND sb + cs >= 20
+)
+SELECT player,
+(sb / sb_attempts)
+FROM my_cte
+
+--I don't know why this isn't working
 
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
